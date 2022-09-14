1d508afb31b395989243f9715e6ab8d956b1d78b
sabrina calvo <calvo.sabrinas@gmail.com>
2022-09-13 21:32:19 -0300

    dom.eventos,json

--- index.js
+++ index.js
@@ -23,122 +23,196 @@
  // Simulador interactivo
 
  // null esta de relleno, para utilizar las posiciones de 1 en adelante.
-const arrHorarios = [null,"18:00", "18:30", "19:00", "19:30", "20:00", "20:30"]
-const arrDias = [null,"Lunes","Martes","Miercoles","Jueves","Viernes"]
-const arrEspecialidades = [null,"Medico Clinico","Traumatologia"]
-const arrMedioPago = [null,"Particular", "Obra Social", "Otros"]
+// const arrHorarios = [null,"18:00", "18:30", "19:00", "19:30", "20:00", "20:30"]
+// const arrDias = [null,"Lunes","Martes","Miercoles","Jueves","Viernes"]
+// const arrEspecialidades = [null,"Medico Clinico","Traumatologia"]
+// const arrMedioPago = [null,"Particular", "Obra Social", "Otros"]
 
- class Persona {
-  constructor(_nombre, _dni) {
-    this.nombre = _nombre,
-      this.dni = _dni,
-      this.mostrar = function () { console.log("Detalles de usuario: " + "\n" + this.nombre + "\n" + this.dni) }
-  }
-}
+//  class Persona {
+//   constructor(_nombre, _dni) {
+//     this.nombre = _nombre,
+//       this.dni = _dni,
+//       this.mostrar = function () { console.log("Detalles de usuario: " + "\n" + this.nombre + "\n" + this.dni) }
+//   }
+// }
 
- class Turno {
-  constructor(dia, horario, especialidad, medioPago) {
-    this.dia = dia,
-      this.horario = horario,
-      this.especialidad = especialidad,
-      this.medioPago = medioPago,
-      this.mostrar = function () { console.log("Detalles de turno: " + "\n" + this.dia + "\n" + this.horario + "\n" + this.medioPago) }
-  }
-}
+//  class Turno {
+//   constructor(dia, horario, especialidad, medioPago) {
+//     this.dia = dia,
+//       this.horario = horario,
+//       this.especialidad = especialidad,
+//       this.medioPago = medioPago,
+//       this.mostrar = function () { console.log("Detalles de turno: " + "\n" + this.dia + "\n" + this.horario + "\n" + this.medioPago) }
+//   }
+// }
 
-// Retorna una lista de items enumerados en STRING con los elementos de un arreglo.
-function crearLista(arrlista) {
-  let lista = "";
-  for(let i = 1; i<arrlista.length; i++) {
-    lista = lista + "\n" + i + " - "+ arrlista[i] // Ej: 1 - Elemento
-  }
-  return lista
-}
+// // Retorna una lista de items enumerados en STRING con los elementos de un arreglo.
+// function crearLista(arrlista) {
+//   let lista = "";
+//   for(let i = 1; i<arrlista.length; i++) {
+//     lista = lista + "\n" + i + " - "+ arrlista[i] // Ej: 1 - Elemento
+//   }
+//   return lista
+// }
 
-// Creo una lista automatica para cada arreglo por si cambio o agrego mas opciones en los arreglos.
-let diasLista = crearLista(arrDias)
-let horariosLista = crearLista(arrHorarios)
-let especialidadesLista = crearLista(arrEspecialidades)
-let medioPagoLista = crearLista(arrMedioPago)
+// // Creo una lista automatica para cada arreglo por si cambio o agrego mas opciones en los arreglos.
+// let diasLista = crearLista(arrDias)
+// let horariosLista = crearLista(arrHorarios)
+// let especialidadesLista = crearLista(arrEspecialidades)
+// let medioPagoLista = crearLista(arrMedioPago)
 
-function solicitarDatos(usuario) {
-  alert("Bienvenidos al PORTAL DEL PACIENTE");
-  do {
-    nombreCompleto = prompt("Ingrese su nombre: ");
-  } while (nombreCompleto === "")
-  usuario.nombre = nombreCompleto
-  do {
-    dni = prompt("Ingrese su DNI: ");
-  } while (dni == "") 
-   usuario.dni = dni
-}
+// function solicitarDatos(usuario) {
+//   alert("Bienvenidos al PORTAL DEL PACIENTE");
+//   do {
+//     nombreCompleto = prompt("Ingrese su nombre: ");
+//   } while (nombreCompleto === "")
+//   usuario.nombre = nombreCompleto
+//   do {
+//     dni = prompt("Ingrese su DNI: ");
+//   } while (dni == "") 
+//    usuario.dni = dni
+// }
 
-function solicitarTurno(turno) {
-  do {
-    especialidad = Number(prompt("Especialidad: " + especialidadesLista));
-  } while (especialidad == "")
-  turno.especialidad = arrEspecialidades[especialidad]
-  do {
-    diaUsuario = Number(prompt("Ingresar Día del turno: " + diasLista));
-  } while (diaUsuario == "")
-  turno.dia = arrDias[diaUsuario]
-  do {
-    horaUsuario = Number(prompt("Ingresar Hora de Turno Disponible: " + horariosLista));
-  } while (horaUsuario == "") 
-  turno.horario = arrHorarios[horaUsuario]
-}
+// function solicitarTurno(turno) {
+//   do {
+//     especialidad = Number(prompt("Especialidad: " + especialidadesLista));
+//   } while (especialidad == "")
+//   turno.especialidad = arrEspecialidades[especialidad]
+//   do {
+//     diaUsuario = Number(prompt("Ingresar Día del turno: " + diasLista));
+//   } while (diaUsuario == "")
+//   turno.dia = arrDias[diaUsuario]
+//   do {
+//     horaUsuario = Number(prompt("Ingresar Hora de Turno Disponible: " + horariosLista));
+//   } while (horaUsuario == "") 
+//   turno.horario = arrHorarios[horaUsuario]
+// }
 
- function realizarPago(turno){
-  let op = Number(prompt("ingrese el medio de pago: " + medioPagoLista));
+//  function realizarPago(turno){
+//   let op = Number(prompt("ingrese el medio de pago: " + medioPagoLista));
   
-  switch (op) {
-    case 1: 
-      turno.medioPago = arrMedioPago[op] + " ¡SE ABONA EN CONSULTORIO!"
-      break;
-    case 2: 
-      turno.medioPago = arrMedioPago[op] + " ¡ACUDIR CON EL CARNET DE SU OBRA SOCIAL!"
-      break;
-    default:
-      turno.medioPago = arrMedioPago[op] + " ¡No se ha registrado metodo de pago, debera abonar en consultorio!"
-      break;
-  }
+//   switch (op) {
+//     case 1: 
+//       turno.medioPago = arrMedioPago[op] + " ¡SE ABONA EN CONSULTORIO!"
+//       break;
+//     case 2: 
+//       turno.medioPago = arrMedioPago[op] + " ¡ACUDIR CON EL CARNET DE SU OBRA SOCIAL!"
+//       break;
+//     default:
+//       turno.medioPago = arrMedioPago[op] + " ¡No se ha registrado metodo de pago, debera abonar en consultorio!"
+//       break;
+//   }
+// }
+
+// function mostrarResumen(usuario, turno) {
+//   alert("Paciente: " + usuario.nombre + "\nDNI: " + usuario.dni + "\nEspecialidad: " + turno.especialidad + "\nFecha del turno: " + turno.dia + "\nHora: " + turno.horario + "Hs" + "\nMedioDePagoSeleccionado: " + turno.medioPago);
+// }
+
+// // Creo un obj Persona y un Turno
+// let usuarioNuevo = new Persona();
+// let turnoNuevo = new Turno();
+// let arrTurnosTotales = [];
+
+// // Creo y agrego Turnos al arreglo de turnosTotales
+// const turno1 = new Turno("Lunes", "18:00", "Traumatologia", "Obra social");
+// const turno2 = new Turno("Miercoles", "19:00", "Medico Clinico", "Particular");
+// const turno3 = new Turno("Viernes", "20:30", "Traumatologia", "Particular");
+// arrTurnosTotales.push(turno1,turno2,turno3);
+
+// /*
+// Comienzo de la simulacion
+// */
+// solicitarDatos(usuarioNuevo);
+// solicitarTurno(turnoNuevo);
+// realizarPago(turnoNuevo);
+// arrTurnosTotales.push(turnoNuevo)
+// mostrarResumen(usuarioNuevo,turnoNuevo);
+
+
+// // Funcion que recibe un parametro arreglo, genera salida por consola de cada elemento, y retorna un mensaje.
+// function imprimirTurnosDados(collection, portal) {
+//   for (item of collection) {
+//     portal(item);
+//   }
+// return "Ya terminamos de listar los turnos por consola"
+// }
+
+// let a = imprimirTurnosDados(arrTurnosTotales,console.log);
+// console.log(a);
+
+const Nombre = document.querySelector(".nombre")
+console.log(Nombre)
+console.log(Nombre.value)
+const Dni = document.querySelector(".dni")
+const Especialidad = document.querySelector(".especialidad")
+const Medico = document.querySelector(".medico")
+const Mediopago = document.querySelector(".mediopago")
+const ButtonAgregarTurno = document.querySelector(".button")
+
+const ListadoTurnos = document.querySelector(".listado-turnos")
+console.log(ListadoTurnos)
+
+const baseDatos = window.localStorage 
+
+ButtonAgregarTurno.onclick = () => {
+    let Contacto = {
+        id: Math.random(1, 100),
+        nombre: Nombre.value,
+        dni: Dni.value,
+        especialidad: Especialidad.value,
+        medico: Medico.value,
+        mediopago: Mediopago.Value,
+    }
+     guardarTurno(baseDatos, Contacto)
+   
+}
+cargarTurnos(baseDatos, ListadoTurnos)
+
+
+class Turno {
+    constructor(nombre, dni, especialidad, mediopago){
+        this.nombre = nombre;
+        this.dni = dni;
+        this.especialidad = especialidad;
+        this.mediopago = mediopago;
+        this.mostrar = function () { console.log("Detalles de usuario: " + "\n" + this.nombre + "\n" + this.dni) }
+    }
 }
 
-function mostrarResumen(usuario, turno) {
-  alert("Paciente: " + usuario.nombre + "\nDNI: " + usuario.dni + "\nEspecialidad: " + turno.especialidad + "\nFecha del turno: " + turno.dia + "\nHora: " + turno.horario + "Hs" + "\nMedioDePagoSeleccionado: " + turno.medioPago);
+const arrTurnos = [
+ new Turno("Maria Perez", "45.321.657","nutricionista", "obrasocial"),
+ new Turno("Juan Lopez", "32.345.678", "traumatologia", "particular"),
+ new Turno("Jose Garcia", "57.645.231", "Médica Clinica", "pami")
+
+]
+
+// esta funcion se encarga de mostrar  todos los turnos
+
+function mostrarTurnos(arrTurnos) {
+
+    const contenedorDeTurnos = document.getElementById("formulario");
+    contenedorDeTurnos.innerHTML = "";
+    // por cada uno
+    arrTurnos.forEach(turno => {
+        const divTurno = document.createElement("div");
+        divTurno.classList.add("turno");
+        divTurno.innerHTML =  `
+        <h1>${turno.nombre}</h2>
+        <h3>${turno.dni}</h3>
+        <p> ${turno.especialidad}</p>
+        <p>${turno.mediopago}</p><br>
+    `;
+
+    // const botonVerDetalle = crearBotonVerDetalle(turno)
+    // divTurno.oppendChild(botonVerDetalle);
+
+    contenedorDeTurnos.appendChild(divTurno);
+    })
 }
-
-// Creo un obj Persona y un Turno
-let usuarioNuevo = new Persona();
-let turnoNuevo = new Turno();
-let arrTurnosTotales = [];
-
-// Creo y agrego Turnos al arreglo de turnosTotales
-const turno1 = new Turno("Lunes", "18:00", "Traumatologia", "Obra social");
-const turno2 = new Turno("Miercoles", "19:00", "Medico Clinico", "Particular");
-const turno3 = new Turno("Viernes", "20:30", "Traumatologia", "Particular");
-arrTurnosTotales.push(turno1,turno2,turno3);
-
-/*
-Comienzo de la simulacion
-*/
-solicitarDatos(usuarioNuevo);
-solicitarTurno(turnoNuevo);
-realizarPago(turnoNuevo);
-arrTurnosTotales.push(turnoNuevo)
-mostrarResumen(usuarioNuevo,turnoNuevo);
+mostrarTurnos(arrTurnos);
 
 
-// Funcion que recibe un parametro arreglo, genera salida por consola de cada elemento, y retorna un mensaje.
-function imprimirTurnosDados(collection, portal) {
-  for (item of collection) {
-    portal(item);
-  }
-return "Ya terminamos de listar los turnos por consola"
-}
 
-let a = imprimirTurnosDados(arrTurnosTotales,console.log);
-console.log(a);
 
 
 
